a3da61c8bdcb1795aa715748931f1d85758839cc
Zontadious <87883897+Superman9393@users.noreply.github.com>
2023-10-27 20:52:17 -0500

    added default hello world

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(cors());
 const PORT = 4545;
 
+app.get("/",(req,res)=>{
+    res.send(<body>Hello World</body>)
+})
 app.listen(PORT,()=>console.log(`listening on port... ${PORT}`));
