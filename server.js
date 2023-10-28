0801fb89b8a25b91a322a6fbad53bdf07d17c081
Zontadious <87883897+Superman9393@users.noreply.github.com>
2023-10-28 03:41:19 -0500

    update

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ const PORT = 4545;
 app.get("/",(req,res)=>{
     res.send(`welcome to the server ${req.headers.host}`)
 })
-app.listen(PORT,()=>console.log(`listening on port... ${PORT}`));
+app.get("/relate",(req,res)=>{
+    res.send("this is the relates page")
+})
+app.listen(PORT,()=>console.log(`listening on port... ${PORT}`))
